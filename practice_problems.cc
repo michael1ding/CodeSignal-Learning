a8b65db1257dad53f42b6a7c8a6c11a56f2482aa
Michael Ding <m28ding@uwaterloo.ca>
2022-12-05 22:33:46 -0500

    wip

--- practice_problems.cc
+++ practice_problems.cc
@@ -373,12 +373,55 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* out = nullptr;
-        while (list1 && list2) {
-
+        ListNode* head = nullptr;
+        while (list1 || list2) {
+            if (!list1) {
+                if (out) {
+                    out->next = list2;
+                    out = out->next;
+                    list2 = list2->next;
+                } else {
+                    out = list2;
+                    head = list2;
+                    list2 = list2->next;
+                }
+            } else if (!list2) {
+                if (out) {
+                    out->next = list1;
+                    out = out->next;
+                    list1 = list1->next;
+                } else {
+                    out = list1;
+                    head = list1;
+                    list1 = list1->next;
+                }
+            } else {
+                if (out) {
+                    if (list1->val < list2->val) {
+                        out->next = list1;
+                        out = out->next;
+                        list1 = list1->next;
+                    } else {
+                        out->next = list2;
+                        out = out->next;
+                        list2 = list2->next;
+                    }
+                } else {
+                    if (list1->val < list2->val) {
+                        out = list1;
+                        head = out;
+                        list1 = list1->next;
+                    } else {
+                        out = list2;
+                        head = out;
+                        list2 = list2->next;
+                    }
+                }
+            }
         }
+        return head;
     }
 };
-
 
 
 
